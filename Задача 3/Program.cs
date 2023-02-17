8451e98b7dd2ebf33808470d3a328295f3718d31
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-17 09:12:41 +0300

    Добавлена Задача 3

---
+++ Задача 3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Даны радиус круга и сторона квадрата. У какой фигуры площадь больше?
+// Площадь круга равна pi*r^2.
+// Пдлощадь квадрата равна s*s.
+
+string buf1;
+Console.WriteLine("Введите значение радиуса круга r:");
+buf1 = Console.ReadLine();
+double r = Convert.ToDouble(buf1);
+
+string buf2;
+Console.WriteLine("Введите значение стороны квадрата s:");
+buf2 = Console.ReadLine();
+double s = Convert.ToDouble(buf2);
+
+
+double pi = 3.14;
+double O = (pi*r)*(pi*r);
+double S = s*s;
+
+if(O > S)
+{
+    Console.WriteLine(" Площадь круга {0} больше площади квадрата {1}", O, S);
+}
+
+else
+{
+    Console.WriteLine("Площадь квадрата {0} бюольше площади круга {1}", S, O);
+}
