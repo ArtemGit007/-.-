9bb50f8338d0ace2aab1cd6291e6aa394d58613b
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-17 06:23:09 +0300

    Добавлена Задача 1

---
+++ Задача 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Даны два различных вещественных числа. Определить:
+//  а) какое из них больше; б) какое из них меньше.
+
+string buf1;
+Console.WriteLine("Введите первое вещественное число х");
+buf1 = Console.ReadLine();
+double x = Convert.ToDouble(buf1); 
+
+string buf2;
+Console.WriteLine("Введите второе вещественное число у");
+buf2 = Console.ReadLine();
+double y = Convert.ToDouble(buf2);
+if(x > y)
+{
+Console.WriteLine("х больше чем у");
+}
+else 
+Console.WriteLine("x меньше, либо равен y");
