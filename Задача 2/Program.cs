94d762aa660d7a9d4e0fbe12056f7f7cfd3e4a59
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-17 08:33:33 +0300

    Добавлена Задача 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Даны действительные числа x и у, не равные друг другу. 
+// Меньшее из этих двух чисел заменить половиной их суммы, 
+// а большее – их удвоенным произведением.
+
+Console.WriteLine("Введите первое число х:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число y:");
+int y = Convert.ToInt32(Console.ReadLine());
+int t = x;
+
+if(x < y)
+{
+    x = (x + y)/2;
+    y = x*y*2;
+    Console.WriteLine("{0}, {1}", x, y);
+}
+
+
+else
+{
+    
+    x = x*y*2;
+    y = (t + y)/2;
+    Console.WriteLine("{0}, {1}", x, y);
+}
+   
+
+ 
+
+
+
+
+
