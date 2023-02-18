fb2e9d6b49c77a7256ecaf461a73e8b35d56ba46
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-18 10:46:32 +0300

    Добавлена Задача 5

---
+++ Задача 5/Program.cs
@@ -0,0 +1,21 @@
+﻿// Известны два расстояния: одно в километрах, другое – в футах (1 фут 0,305 м). Какое из расстояний меньше?
+
+string buf1;
+Console.WriteLine("Введите первое расстояние в км.");
+buf1 = Console.ReadLine();
+double km = Convert.ToDouble(buf1);
+
+string buf2;
+Console.WriteLine("Введите второе расстояние в футах");
+buf2 = Console.ReadLine();
+double ft = Convert.ToDouble(buf2);
+
+if(km/0.305 < ft)
+{
+    Console.WriteLine("Расстояние в {0} киллометра(ов) меньше, чем {1} фута(ов) ", km, ft);
+}
+
+else 
+{
+   Console.WriteLine("Расстояние в {0} киллометра(ов) больше, чем расстояние в {1} фута(ов) ", km, ft);
+}
