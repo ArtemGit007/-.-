411d227d2a0c63a6669e9cc170bff6e00f2df915
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-18 10:27:59 +0300

    Добавлены изм. в Задачу 4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,40 @@
+﻿// Даны три действительных числа. Возвести в квадрат те из них, 
+// значения которых неотрицательны, и в четвертую степень – отрицательные.
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первое число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первое число:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > 0)
+{
+   a = a * a;
+   Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Отрицательное число в 4 степени = "  + Math.Pow(a, 4));
+}
+
+if( b > 0)
+{
+   Console.WriteLine("Квадрат числа = " + Math.Pow(b, 2));
+}
+else
+{
+    Console.WriteLine("Отрицательное число в 4 степени = " + Math.Pow(b, 4));
+}
+
+if(c > 0)
+{
+   Console.WriteLine("Квадрат числа = " + Math.Pow(c, 2));
+}
+else
+{
+    Console.WriteLine("Отрицательное число в 4 степени = " + Math.Pow(c, 4));
+
+}
