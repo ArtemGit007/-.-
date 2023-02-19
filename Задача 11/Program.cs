0257f0f805eb997c82a909cb2b4a528d0b72ffce
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-19 19:26:34 +0300

    Добавлена Задача 11

---
+++ Задача 11/Program.cs
@@ -0,0 +1,25 @@
+﻿// Определить максимальное и минимальное значения 
+// из двух различных вещественных чисел.
+
+string buf1;
+Console.WriteLine("Введите первое вещественное число а:"); 
+buf1 = Console.ReadLine();
+double a = Convert.ToDouble(buf1);
+
+string buf2;
+Console.WriteLine("Введите первое вещественное число b:"); 
+buf2 = Console.ReadLine();
+double b = Convert.ToDouble(buf2);
+
+if(a > b)
+{
+    Console.WriteLine("Число {0} больше числа {1}.", a, b);
+}
+else
+{
+    Console.WriteLine("Число {0} больше числа {1}.", b, a);
+}
+
+
+
+ 
