59a329642e30904a5dfdd5f0152de8a14335c7e8
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-21 18:31:09 +0300

    Добавлена Задача 13

---
+++ Задача 13/Program.cs
@@ -0,0 +1,31 @@
+﻿ // Дано двузначное число. Определить: а) кратна ли трем сумма его цифр; 
+// б) кратна ли сумма его цифр числу а.
+
+Console.WriteLine("Введите двузначное число i:"); 
+int i = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите двузначное число a:"); 
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = i/10;
+int c = i%10;
+
+if((c + b)%3 == 0)
+{
+    Console.WriteLine("Сумма цифр числа кратно трем");
+}
+
+else 
+{
+    Console.WriteLine("Сумма цифр не кратна трем");
+}
+
+if((b + c)%a == 0)
+{
+    Console.WriteLine("Сумма цифр кратна {0}", a);
+}
+
+else 
+{
+    Console.WriteLine("Сумма цифр не кратна {0}", a);
+}
