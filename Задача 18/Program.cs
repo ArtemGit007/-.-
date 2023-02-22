865335893891fa22ad2e77b9f856b6bd9aa840d9
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-23 00:50:05 +0300

    Добавлена Задача 18

---
+++ Задача 18/Program.cs
@@ -0,0 +1,23 @@
+﻿// Даны объемы и массы двух тел из разных материалов. 
+// Материал какого из тел имеет большую  плотность?
+// p = m/V
+
+Console.WriteLine("Введите массу первого тела");
+int m1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  объем первого тела");
+int v1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите массу второго тела");
+int m2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  объем второго тела");
+int v2 = Convert.ToInt32(Console.ReadLine());
+
+
+if(m1/v1 < m2/v2)
+{
+    Console.WriteLine("Материал второго тела имеет большую плотность");
+}
+else 
+{
+    Console.WriteLine("Материал первого тела имеет большую плотность");
+}
