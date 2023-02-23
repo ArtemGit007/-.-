b7b30d6cf458f54d88ea07509cc711f9ff8959c1
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-23 21:14:16 +0300

    Добавлена Задача 22

---
+++ Задача 22/Program.cs
@@ -0,0 +1,43 @@
+﻿// Написать программу нахождения суммы большего и меньшего из 3-xчисел.
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b&&a > c&&c < b)
+{
+    int x = a + c;
+    Console.WriteLine("Сумма большего и меньшего из 3-x чисел равна {0}", x);
+}
+if(a > b&&a > c&&b < c)
+{
+    int x = a + b;
+    Console.WriteLine("Сумма большего и меньшего из 3-x чисел равна {0}", x); 
+}
+if(b > a&&b > c&&c < a)
+{
+    int x = b + c;
+    Console.WriteLine("Сумма большего и меньшего из 3-x чисел равна {0}", x); 
+}
+if(b > a&&b > c&&a < c)
+{
+    int x = b + a;
+    Console.WriteLine("Сумма большего и меньшего из 3-x чисел равна {0}", x); 
+}
+
+if(c > b&&c > a&&b < a)
+{
+    int x = b + c;
+    Console.WriteLine("Сумма большего и меньшего из 3-x чисел равна {0}", x); 
+}
+if(c > b&&c > a&&a < b)
+{
+    int x = c + a;
+    Console.WriteLine("Сумма большего и меньшего из 3-x чисел равна {0}", x); 
+}
+
+
+
