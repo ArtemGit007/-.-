df17ff44b967ee6c25779579695fec6b89216839
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-23 21:44:36 +0300

    Добавлена Задача 25

---
+++ Задача 25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано трехзначное число. Верно ли, что все его цифры одинаковые?
+
+Console.WriteLine("Введите трёхзначное число:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int a = x/100;
+int b = (x%100)/10;
+int c = x%10;
+
+if(a == b&&b == c)
+{
+    Console.WriteLine("Все цифры числа {0} равны", x);
+}
+else
+
+{
+    Console.WriteLine("Цифры числа {0} различны", x);
+}
