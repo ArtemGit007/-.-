e4303ebe058984d0ed7e0c14bfb85069de01a2cb
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-23 21:23:00 +0300

    Добавлена Задача 21

---
+++ Задача 21/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу является ли число a делителем числа b?
+
+Console.WriteLine("Введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b % a == 0)
+{
+    Console.WriteLine("Число {0} является делителем числа {1}", a, b);
+}
+else
+{
+    Console.WriteLine("Число {0} не является делителем числа {1}", a, b);
+}
