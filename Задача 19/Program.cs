1a94aac0aa7c5e3349a2a06f20a0cc6993a3c7d0
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-23 18:54:55 +0300

    Добавлена Задача 19

---
+++ Задача 19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Если целое число m делится нацело на целое число n, то вывести на экран частное от деления, 
+// в противном случае вывести сообщение «m на n нацело не делится».
+
+Console.WriteLine("Введите целое число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int c  = m/n;
+
+if(m % n == 0)
+{
+    Console.WriteLine("Частное от деления {0} на {1} раввно {2}", m, n, c);
+}
+else 
+{
+    Console.WriteLine("{0} на {1} без остатка не делится", m, n);
+}
