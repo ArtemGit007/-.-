b03d11f463d9f7754e9656c02b1d4ca8745d0844
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-24 20:31:56 +0300

    добавлена Задача 30

---
+++ Задача 30/Program.cs
@@ -0,0 +1,22 @@
+﻿//   Дано трехзначное число. Определить: кратна ли сумма его цифр числу а. 
+
+Console.WriteLine("Введите трёхзначное число:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int i = x/100;
+int b = (x%100)/10;
+int c = x%10;
+
+int y = i + b + c;
+
+if(y%a == 0)
+{
+    Console.WriteLine("Сумма цифр числа {0} равна {1} и кратна числу {2}", x, y, a);
+}
+else
+{
+    Console.WriteLine("Сумма цифр числа {0} равна {1} и не кратна числу {2} ", x, y, a); 
+}
