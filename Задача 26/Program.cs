3289576b8e18c0031b68ba4323509a860948de08
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-24 19:52:49 +0300

    Добавлена Задача 26

---
+++ Задача 26/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Дано трехзначное число. Определить, есть ли среди его цифр одинаковые.
+
+
+
+Console.WriteLine("Введите трёхзначное число:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int a = x/100;
+int b = (x%100)/10;
+int c = x%10;
+
+if(a==b||a==c||b==c)
+{
+    Console.WriteLine("Среди цифр числа {0} есть одинаковые", x);
+}
+else
+{
+    Console.WriteLine("Все цифры числа {0} разные", x);
+}
