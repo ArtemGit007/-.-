474c19376e58a3f30b4be3d63e9ee178afcc27a1
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-25 19:03:20 +0300

    Добавлена Задача 30

---
+++ Задача 31/Program.cs
@@ -0,0 +1,11 @@
+﻿// Заполнить массив случайными рандомными числами от 0 до 36, длина массива 10
+Random r = new Random();
+int size = 10;
+int[] arr = new int[size];
+for (int i = 0; i < size; i++)
+{
+    arr[i] = r.Next(0, 36);
+    Console.Write(arr[i] + " ");
+}
+
+
