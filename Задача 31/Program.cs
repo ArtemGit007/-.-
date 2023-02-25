f57617a9e44f31c334ba47e4b8bbe6afad493698
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-25 19:08:23 +0300

    Внесены изменения в Задачу 31

--- Задача 31/Program.cs
+++ Задача 31/Program.cs
@@ -1,11 +1,12 @@
 ﻿// Заполнить массив случайными рандомными числами от 0 до 36, длина массива 10
+Console.WriteLine("Введите число спинов n:");
+int n = Convert.ToInt32(Console.ReadLine());
 Random r = new Random();
-int size = 10;
+int size = n;
 int[] arr = new int[size];
 for (int i = 0; i < size; i++)
 {
     arr[i] = r.Next(0, 36);
     Console.Write(arr[i] + " ");
 }
-
 
