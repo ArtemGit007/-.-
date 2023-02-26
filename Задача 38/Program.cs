572e8f61da2ef0f5d23b4c41d59caef6b8894a62
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-26 19:31:55 +0300

    Добавлена Задача 38

---
+++ Задача 38/Program.cs
@@ -0,0 +1,39 @@
+﻿// Даны целые числа m, n. Если числа не равны, то заменить каждое из них 
+// одним и тем же числом, равным большему из исходных, 
+// а если равны, то заменить числа нулями. 
+
+Console.WriteLine("Введите значение числа m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение числа n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+if( n !=  m)
+if(n > m)
+{
+    m = n;
+    Console.WriteLine("Большее из исходных число {0}", n);
+}
+else 
+{
+    n = m;
+    Console.WriteLine("Большее из исходных число {0}", m); 
+}
+
+else
+{
+    n = 0;
+    m = 0;
+    Console.WriteLine("Числа равны, им  присвоено значение {0}.", n);
+}
+
+
+
+
+
+
+
+
+
+
+
