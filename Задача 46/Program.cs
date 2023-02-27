4dd3d8fb018e64d0ca184152e83202b9e9780a5d
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-27 22:38:12 +0300

    обавлена адача 46

---
+++ Задача 46/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Написать программу определени: является ли треугольник со сторонами a, b, с равносторонним.
+
+Console.WriteLine("Введите значение стороны а:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение стороны b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение стороны c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+if(a == b&&b == c&&c == a)
+{
+    Console.WriteLine("Треугольник равносторонний");
+}
+else
+{
+    Console.WriteLine("Треугольник не равносторонний");
+}
