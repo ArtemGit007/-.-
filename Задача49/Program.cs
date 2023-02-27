84386642b75a25ae248c233349a11d5259713ee4
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-27 23:56:07 +0300

    Добавлена Задача 49

---
+++ Задача49/Program.cs
@@ -0,0 +1,26 @@
+﻿// Подсчитать количество целых чисел среди чисел a, b, с.
+
+string buf1;
+Console.WriteLine("Введите число а:");
+buf1 =Console.ReadLine();
+double a = Convert.ToDouble(buf1);
+
+string buf2;
+Console.WriteLine("Введите число b:");
+buf2 = Console.ReadLine();
+double b = Convert.ToDouble(buf2);
+
+string buf3;
+Console.WriteLine("Введите число c:");
+buf3 = Console.ReadLine();
+double c = Convert.ToDouble(buf3);
+
+double t = 0;
+if(a%1==0)
+++t;
+if(b%1 == 0)
+++t;
+if(c%1 == 0)
+++t;
+Console.WriteLine("Целых чисел {0}", t);
+
