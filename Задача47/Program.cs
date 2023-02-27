c10080a77ec0a5cde7d3f2b523b23777aaf52925
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-27 23:39:15 +0300

    Добавлена Задача 47

---
+++ Задача47/Program.cs
@@ -0,0 +1,17 @@
+﻿// Подсчитать количество отрицательных чисел среди чисел а, b, с.
+
+Console.WriteLine("Введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int t = 0;
+if(a < 0)
+++t;
+if(b < 0)
+++t;
+if(c <0)
+++t;
+Console.WriteLine("Отрицательных чисел - {0}", t);
