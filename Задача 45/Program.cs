ada66e0a92f8bd26f70663ec52bdd98c450a9120
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-27 22:34:37 +0300

    Добавлена Задача 45

---
+++ Задача 45/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написатьпрограмму определения - является ли 
+// треугольник со сторонами a, b, с равнобедренным.
+
+Console.WriteLine("Введите значение стороны а:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение стороны b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение стороны c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+if(a == b|b == c|c == a)
+{
+    Console.WriteLine("Треугольник равнобедренный");
+}
+else
+{
+    Console.WriteLine("Треугольник не равнобедренный");
+}
