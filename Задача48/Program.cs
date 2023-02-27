64de5f09544d80ff8591399821729d56a466e2ae
Artem007 <artem.zagrebelny@yandex.ru>
2023-02-27 23:41:54 +0300

    Добавлена Задача 48

---
+++ Задача48/Program.cs
@@ -0,0 +1,21 @@
+﻿// Подсчитать колличество положительных чисел среди чисел а, b, c.
+
+
+Console.WriteLine("Введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+int t = 0;
+if(a > 0)
+++t;
+if(b > 0)
+++t;
+if(c > 0)
+++t;
+Console.WriteLine("Положительных чисел {0}", t);
+
+  
