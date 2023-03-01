9945cf09b18c1df01f9fb8324c6aa16788169dab
Artem007 <artem.zagrebelny@yandex.ru>
2023-03-02 01:59:08 +0300

    Добавлена Задача 60

---
+++ Задача 60/Program.cs
@@ -0,0 +1,20 @@
+﻿// Составить программу, которая в зависимости от порядкового номера 
+// месяца (1, 2, ..., 12) выводит на экран количество дней 
+// в этом месяце. Рассмотреть случай, когда год может быть 
+// високосным (информация об этом вводится с клавиатуры).
+
+Console.WriteLine("Введите номер месяца:");
+int month = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Высокосный ли год: если да введи 1, нет введи 2;");
+int year = Convert.ToInt32(Console.ReadLine());
+
+if(month == 1||month == 3||month == 5||month == 7||month == 8||month == 10||month == 12)
+Console.WriteLine("В месяце 31 день.");
+if(month == 4||month == 6||month == 9||month == 11)
+Console.WriteLine("В месяце 30 дней.");
+if(month == 2&&year ==1)
+Console.WriteLine("В месяце 29 дней.");
+if(month == 2&&year ==2)
+Console.WriteLine("В месяце 28 дней.");
+if(month > 12)
+Console.WriteLine("Введен неверный номер месяца.");
