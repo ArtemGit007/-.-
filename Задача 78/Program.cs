04db04037d2595a9e080259da436cff0e302c296
Artem007 <artem.zagrebelny@yandex.ru>
2023-03-04 05:45:10 +0300

    Добавлена Задача 78

---
+++ Задача 78/Program.cs
@@ -0,0 +1,25 @@
+﻿  // Дано целое число k (1 <= k <= 365). 
+//Написать программу определения: каким днем недели
+// (понедельником, вторником, ..., субботой или воскресеньем) 
+// является k-й день невисокосного года, 
+// в котором 1-ое  января - понедельник.
+
+Console.WriteLine("Введите число от 1 до 365:");
+int weekday = Convert.ToInt32(Console.ReadLine());
+
+if((weekday == 1|(weekday)%7 == 1)&&weekday < 366)
+Console.WriteLine("Погедельник.");
+if((weekday == 2|(weekday)%7 == 2)&&weekday < 366)
+Console.WriteLine("Вторник.");
+if((weekday == 3|(weekday)%7 == 3)&&weekday < 366)
+Console.WriteLine("Среда.");
+if((weekday == 4|(weekday)%7 == 4)&&weekday < 366)
+Console.WriteLine("Четверг ");
+if((weekday == 5|(weekday)%7 == 5)&&weekday < 366)
+Console.WriteLine("Пятница.");
+if((weekday == 6|(weekday)%7 == 6)&&weekday < 366)
+Console.WriteLine("Суббота .");
+if((weekday == 7|(weekday)%7 == 0)&&weekday < 366)
+Console.WriteLine("Воскресенье.");
+if(weekday > 366)
+Console.WriteLine("Введено некорректное значение.");
