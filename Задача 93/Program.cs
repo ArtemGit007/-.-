8fed0cae57652e3456b697b35b8416e8dcd5f90c
Artem007 <artem.zagrebelny@yandex.ru>
2023-03-07 12:45:57 +0300

    Добавлена Задача 93

---
+++ Задача 93/Program.cs
@@ -0,0 +1,12 @@
+﻿//Написать программу, позволяющую по последней цифре числа
+//  определить последнюю цифру его квадрата.
+
+string buf;
+Console.WriteLine("Введите k-значное число:");
+buf = Console.ReadLine();
+double x = Convert.ToDouble(buf);
+
+
+//double c = x%10;
+double a = Math.Pow(x%10, 2);
+Console.WriteLine("Квадрат последней цифры заданного числа равен {0}", a);
