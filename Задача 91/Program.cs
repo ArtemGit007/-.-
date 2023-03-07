ae4661388f8ed9d46ef1a39c194fbf971bc675fb
Artem007 <artem.zagrebelny@yandex.ru>
2023-03-07 11:22:47 +0300

    Добавлена Задача 91

---
+++ Задача 91/Program.cs
@@ -0,0 +1,30 @@
+﻿// Для каждой введенной цифры (0-9) вывести соответствующее ей название 
+// на английском языке (0 – zero, 1 – one, 2 – two, ...).
+
+Console.WriteLine("Введите цифру от 0 до 9:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if(x == 0)
+Console.WriteLine("Zero.");
+if(x == 1)
+Console.WriteLine("One.");
+if(x == 2)
+Console.WriteLine("Two.");
+if(x == 3)
+Console.WriteLine("Three.");
+if(x == 4)
+Console.WriteLine("Four.");
+if(x == 5)
+Console.WriteLine("Five.");
+if(x == 6)
+Console.WriteLine("Six.");
+if(x == 7)
+Console.WriteLine("Seven.");
+if(x == 8)
+Console.WriteLine("Eight.");
+if(x == 9)
+Console.WriteLine("Nine.");
+if(x > 10)
+Console.WriteLine("Mistake.");
+
+
