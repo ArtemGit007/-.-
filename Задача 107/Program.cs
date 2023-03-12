59cead123bace44a92ca44b6aef6b7f539300162
Artem007 <artem.zagrebelny@yandex.ru>
2023-03-12 09:06:39 +0300

    Добавлена Задача 107

---
+++ Задача 107/Program.cs
@@ -0,0 +1,11 @@
+﻿// Составить программу, позволяющую по последней цифре данного числа определить 
+// последнюю цифру куба этого числа.
+
+string buf;
+Console.WriteLine("Введите число:");
+
+buf = Console.ReadLine();
+double x = Convert.ToDouble(buf);
+
+double a = Math.Pow((x%10), 3);
+Console.WriteLine("Куб последней цифры числа {0} равен {1}", x, a);
